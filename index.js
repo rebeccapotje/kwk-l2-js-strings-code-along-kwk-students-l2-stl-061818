224f8753a370feebcdd3aba7aa004064090273a7
rebeccapotje <rebecca.potje@gmail.com>
2018-06-20 21:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let currentUser = "Mia"
 
+let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = 'Welcome, '
 
