defd565f94f03fec4c4e59aef2816541a817579e
rebeccapotje <rebecca.potje@gmail.com>
2018-06-20 21:23:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// Write your code in this file!
+let currentUser = "Rebecca"
+
+let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0) + "!"
 
